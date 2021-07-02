83df28b25949a752e28f0263ff48d684d8cc880e
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2021-07-02 01:08:55 -0500

    01_05e

--- src/WisdomPetMedicine.Rescue.Api/Infrastructure/RescueDbContext.cs
+++ src/WisdomPetMedicine.Rescue.Api/Infrastructure/RescueDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WisdomPetMedicine.Rescue.Api.IntegrationEvents;
 using WisdomPetMedicine.Rescue.Domain.Entities;
 
 namespace WisdomPetMedicine.Rescue.Api.Infrastructure
@@ -12,6 +13,7 @@ namespace WisdomPetMedicine.Rescue.Api.Infrastructure
         
         public DbSet<Adopter> Adopters { get; set; }
         public DbSet<RescuedAnimal> RescuedAnimals { get; set; }
+        public DbSet<PetFlaggedForAdoptionIntegrationEvent> RescuedAnimalsMetadata { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlaggedForAdoptionIntegrationEventHandler.cs
+++ src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlaggedForAdoptionIntegrationEventHandler.cs
@@ -1,21 +1,29 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using WisdomPetMedicine.Rescue.Api.Infrastructure;
+using WisdomPetMedicine.Rescue.Domain.Entities;
+using WisdomPetMedicine.Rescue.Domain.Repositories;
+using WisdomPetMedicine.Rescue.Domain.ValueObjects;
 
 namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
 {
     public class PetFlaggedForAdoptionIntegrationEventHandler : BackgroundService
     {
+        private readonly IServiceScopeFactory serviceScopeFactory;
         private readonly ILogger<PetFlaggedForAdoptionIntegrationEventHandler> logger;
         private readonly ServiceBusClient client;
         private readonly ServiceBusProcessor processor;
         public PetFlaggedForAdoptionIntegrationEventHandler(IConfiguration configuration,
+                                                            IServiceScopeFactory serviceScopeFactory,
                                                             ILogger<PetFlaggedForAdoptionIntegrationEventHandler> logger)
         {
+            this.serviceScopeFactory = serviceScopeFactory;
             this.logger = logger;
 
             client = new ServiceBusClient(configuration["ServiceBus:ConnectionString"]);
@@ -38,7 +46,13 @@ namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
             var body = args.Message.Body.ToString();
             var theEvent = JsonConvert.DeserializeObject<PetFlaggedForAdoptionIntegrationEvent>(body);
             await args.CompleteMessageAsync(args.Message);
-            logger?.LogInformation(body);
+
+            using var scope = serviceScopeFactory.CreateScope();
+            var repo = scope.ServiceProvider.GetRequiredService<IRescueRepository>();
+            var dbContext = scope.ServiceProvider.GetRequiredService<RescueDbContext>();
+            dbContext.RescuedAnimalsMetadata.Add(theEvent);
+            var rescuedAnimal = new RescuedAnimal(RescuedAnimalId.Create(theEvent.Id));
+            await repo.AddRescuedAnimalAsync(rescuedAnimal);
         }
 
         private Task Processor_ProcessErrorAsync(ProcessErrorEventArgs args)
