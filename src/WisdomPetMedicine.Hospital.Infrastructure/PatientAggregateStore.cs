9a2237ccdbb23e3e09937fbd98055984e90ea9ed
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2021-07-11 23:46:07 -0500

    03_02e

---
+++ src/WisdomPetMedicine.Hospital.Infrastructure/PatientAggregateStore.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using WisdomPetMedicine.Hospital.Domain.Entities;
+using WisdomPetMedicine.Hospital.Domain.Repositories;
+using WisdomPetMedicine.Hospital.Domain.ValueObjects;
+
+namespace WisdomPetMedicine.Hospital.Infrastructure
+{
+    public class PatientAggregateStore : IPatientAggregateStore
+    {
+        private readonly CosmosClient cosmosClient;
+        private readonly Container patientContainer;
+        public PatientAggregateStore(IConfiguration configuration)
+        {
+            var connectionString = configuration["CosmosDb:ConnectionString"];
+            var databaseId = configuration["CosmosDb:DatabaseId"];
+            var containerId = configuration["CosmosDb:ContainerId"];
+
+            var clientOptions = new CosmosClientOptions()
+            {
+                SerializerOptions = new CosmosSerializationOptions()
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                }
+            };
+
+            cosmosClient = new CosmosClient(connectionString, clientOptions);
+            patientContainer = cosmosClient.GetContainer(databaseId, containerId);
+        }
+        public Task<Patient> LoadAsync(PatientId patient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
