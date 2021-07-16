6a2a01036bd147557c563e2219c37a6e10a3c5ee
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2021-07-16 00:26:08 -0500

    03_06e

---
+++ src/WisdomPetMedicine.Hospital.Api/Controllers/PatientQueryController.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WisdomPetMedicine.Hospital.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PatientQueryController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public PatientQueryController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string sql = @"SELECT pm.*, p.BloodType, p.Weight, p.Status, p.UpdatedOn
+                            FROM PatientsMetadata pm 
+                            JOIN Patients p
+                            ON pm.Id = p.Id";
+            using var connection = new SqlConnection(configuration.GetConnectionString("Hospital"));
+            var orderDetail = (await connection.QueryAsync(sql)).ToList();
+            return Ok(orderDetail);
+        }
+    }
+}
